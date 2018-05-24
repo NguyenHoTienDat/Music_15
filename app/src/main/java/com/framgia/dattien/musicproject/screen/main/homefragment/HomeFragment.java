beefd4330411fa6815a79132b1c19c0ccab52f8e
NguyenHoTienDat <nguyen.ho.tien.dat@framgia.com>
2018-05-24 08:50:53 +0700

    Create home ui and Add indef main activity

--- app/src/main/java/com/framgia/dattien/musicproject/screen/main/homefragment/HomeFragment.java
+++ app/src/main/java/com/framgia/dattien/musicproject/screen/main/homefragment/HomeFragment.java
@@ -1,5 +1,11 @@
 package com.framgia.dattien.musicproject.screen.main.homefragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.framgia.dattien.musicproject.R;
 import com.framgia.dattien.musicproject.screen.BaseFragment;
 
 /**
@@ -11,6 +17,12 @@ public class HomeFragment extends BaseFragment {
     public static final String HOME_FRG_TAG = "HomeFragment";
 
     private static HomeFragment mNewInstance;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
 
     public static HomeFragment getInstance() {
         if (mNewInstance == null) {
